36a69b1d38fdd048a2d83ea7996095543e0cf917
odogwukelly <odogwukellyice@gmail.com>
2023-05-02 18:31:42 -0700

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,16 +2,16 @@
 
 /**
  * swap_ints - Swap two integers in an array.
- * @x: The first integer to swap.
- * @y: The second integer to swap.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swap_ints(int *x, int *y)
+void swap_ints(int *a, int *b)
 {
 	int tmp;
 
-	tmp = *x;
-	*a = *y;
-	*y = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -23,7 +23,7 @@ void swap_ints(int *x, int *y)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t p, len = size;
+	size_t i, len = size;
 	bool bubbly = false;
 
 	if (array == NULL || size < 2)
@@ -32,11 +32,11 @@ void bubble_sort(int *array, size_t size)
 	while (bubbly == false)
 	{
 		bubbly = true;
-		for (p = 0; p < len - 1; p++)
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[p] > array[p + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + p, array + p + 1);
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
